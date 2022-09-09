a52bb4601691903ec29e921558ec4be05ef3c9bd
Edivaldo Almeida <edivaldo.va@gmail.com>
2022-09-08 22:15:30 -0300

    Other component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+
 
 function App() {
-return(
-  <>
-  <h1>Olá mundo</h1>
-  <NotificationButton />
-  </>
-)
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-contrainer">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default App
